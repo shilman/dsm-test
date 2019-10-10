42c95672775e9ae95301006e760915342d230685
Michael Shilman <michael@lab80.co>
2019-10-10 15:51:41 +0800

    Add storybook-dsm

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import { initDsm } from "@invisionapp/dsm-storybook";
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// if (process.env.STORYBOOK_DSM) {
+initDsm({
+  addDecorator,
+  addParameters,
+  callback: () =>
+    configure(require.context("../src/stories", true, /\.stories\.js$/), module)
+});
+// } else {
+//   configure(require.context("../src/stories", true, /\.stories\.js$/), module);
+// }
